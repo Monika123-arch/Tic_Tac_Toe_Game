7f08dbad40d7bfa3e9a52fa422de58d86ba83e54
Monika <monikadkale98@gmail.com>
2021-02-21 09:34:51 +0530

    added new program

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,16 +1,35 @@
+import java.util.*;
 public class TicTacToe {
+	
+	//Static Variables
+	public static char[] board = new char[10];
+	public static char userChoice, compChoice; 	
 
-    //Static Variables
-    public static char[] board = new char[10];
+	//CreateBoard() method to initialise a char array of size 10 to be a blank space
+	public static void createBoard() {
+		for (int i = 1; i < 10; i ++) {
+			board[i] = ' ';
+		}
+	}
 
-    //CreateBoard() method to initialise a char array of size 10 to be a blank space
-    public static void createBoard() {
-        for (int i = 1; i < 10; i ++) {
-            board[i] = ' ';
-        }
-    }
+	//playerChoice() method to choose User's and Computer's choice of X and O
+	public static void playerChoice() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose letter 'X' or 'O': ");
+		userChoice = sc.next().charAt(0);
+		if ( userChoice == 'X' || userChoice == 'x') {
+			userChoice = 'X';
+			compChoice = 'O';
+		}
+		else {
+			userChoice = 'O';
+			compChoice = 'X';
+		}
+		System.out.println("User: "+userChoice+"\nComputer: "+compChoice);
+	}
 
-    public static void main(String[] args) {
-        createBoard();
-    }
+	public static void main(String[] args) {
+		createBoard();
+		playerChoice();
+	}
 }
